c62521e76eb2a310fea2cf1df173d2e9e78ed460
Guillaume Roussin <guillaume.roussin44@gmail.com>
2022-11-01 23:45:35 +0100

    added proejct structure

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from loi_mouvement import LoiMouvement
+
+def main():
+    # v_max = 4
+    # distance_totale = 10
+
+    # loi = LoiMouvement(v_max, distance_totale)
+    # loi.get_vitesse()
+    # loi.get_distance()
+    # loi.get_acceleration()
+    # loi.afficher(te)
+    pass
+
+if __name__ == "__main__":
+    main()
